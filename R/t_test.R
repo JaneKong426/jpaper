98608f03941f5428102f624b6c48568177eadebe
Dongdong Kong <kongdd@users.noreply.github.com>
2020-04-06 17:24:45 +0800

    Initial commit

---
+++ R/t_test.R
@@ -0,0 +1,61 @@
+label_sd <- function (x, ...)
+{
+    x <- as.numeric(x)
+    x <- x[!is.na(x)]
+    y <- mean(x)
+    sd <- sd(x)
+
+    sprintf("%.2f±%.2f", y, sd)
+    # c(y = y, ymin = y - sd, ymax = y + sd, sd = sd)
+}
+
+length_valid <- function(.) {
+    sum(!is.na(as.numeric(.)))
+}
+
+#' T test and TukeyHSD test
+#'
+#' @param x A numeric vector
+#' @param y A numeric vector
+#'
+#' @seealso [stats::t.test()], [stats::TukeyHSD()]
+#'
+#' @import data.table
+#' @export
+t_test <- function(x, y) {
+    # formula <- sprintf("%s~order", varname) %>% as.formula()
+    # m <- aov(formula, df)
+    # s <- TukeyHSD(m)
+    d <- rbind(data.table(variable = "x", value = x), data.table(variable= "y", value = y))
+    m <- aov(value ~ variable, d)
+    s <- TukeyHSD(m)[[1]]
+
+    # x <- df[order == "first"][[varname]]
+    # y <- df[order == "second"][[varname]]
+
+    r <- t.test(x, y)
+    tval <- r$statistic[[1]]
+    pvalue <- r$p.value
+
+    first <- label_sd(x)
+    second <- label_sd(y)
+
+    data.table(first, second, s, tval, pvalue)
+}
+
+#' @param file The file path of xlsx
+#'
+#' @examples
+#' \dontrun{
+#' t_test(x, y)
+#' t_test_simple("a.xlsx")
+#' }
+#' @export
+#' @importFrom openxlsx read.xlsx write.xlsx
+#' @rdname t_test
+t_test_simple <- function(file, outfile = NULL) {
+    d <- read.xlsx(file)
+    ans <- t_test(d[[1]], d[[2]])
+    if (!is.null(outfile)) write.xlsx(ans, outfile)
+    ans
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+
+# jpaper
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+The goal of jpaper is to ...
+
+## Installation
+
+You can install the released version of jpaper from [CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("jpaper")
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+library(jpaper)
+## basic example code
+```
+
